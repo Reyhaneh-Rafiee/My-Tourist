82458acf3a619e9f57c296e5d31dc12e51dc9383
MelikaMehrabian <meliika.mehrabiian@gmail.com>
2025-11-24 17:55:41 +0330

    menu & footer

---
+++ js/script.js
@@ -0,0 +1,53 @@
+/********************************************* search box *********************************************/
+//با کلیک روی دکمه سرچ، سرچ باکس باز شود
+const searchBtn = document.querySelector(".search-btn");
+const searchBox = document.querySelector("input[type='search']");
+
+searchBtn.addEventListener("click", function() {
+  searchBox.focus();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
