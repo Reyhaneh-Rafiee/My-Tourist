3250af383789c625fa52dc247aa7f4c4485f6d43
MelikaMehrabian <meliika.mehrabiian@gmail.com>
2025-11-24 20:31:46 +0330

    back to top button

--- js/script.js
+++ js/script.js
@@ -38,6 +38,21 @@ function showSlides(n) {
 
 
 
+/*********************************back to top page button*********************************************/
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn();
+    } else {
+      $('.back-to-top').fadeOut();
+    }
+  });
+  $('.back-to-top').click(function(){
+    $("html, body").animate({ scrollTop: 0 }, 1000);
+    return false;
+  });
+});
+
 
 
 
